7962c17b9ab33d521e1aced7adaa5c86405c489c
sakh271384 <adnan.khan@elektrobit.com>
2019-01-11 12:40:31 +0100

    first commit

---
+++ build/precache-manifest.41ba2f0e3aac376a493d3692325507e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5e2abbe5d316ad0dc1bc",
+    "url": "/static/js/main.5e2abbe5.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "5e2abbe5d316ad0dc1bc",
+    "url": "/static/css/main.3d68446f.chunk.css"
+  },
+  {
+    "revision": "cd9bcf87ab415af62b2fe27eb21221c7",
+    "url": "/index.html"
+  }
+];
